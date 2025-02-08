290c4dbddf71ee16d2d05e3b9c943277213bf260
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2025-02-08 16:57:05 +0330

    start projext tido list

---
+++ 144-explain-of-todolist-mini-project/source/js/script.js
@@ -0,0 +1,34 @@
+﻿let $=document;
+let inputTodo=$.querySelector('.form-control');
+let fathetTodo=$.querySelector('.list-group')
+//  console.log(fathetTodo,inputTodo) //for test selects
+
+
+inputTodo.addEventListener('keydown',function(event){
+    if(event.keyCode=='13'){
+        event.preventDefault()
+        
+        let newLiFather=$.createElement('li');
+        newLiFather.className='list-group-item d-flex justify-content-between align-items-center';
+        
+        let newSpan=$.createElement('span')
+        newSpan.innerHTML=event.target.value
+        
+        let newIcon=$.createElement('i')
+        newIcon.className='fa fa-trash-o delete'
+
+        newLiFather.append(newSpan,newIcon)
+        fathetTodo.append(newLiFather)
+
+        console.log(newLiFather)
+        // console.log(event.target.value,newLi)
+
+
+        inputTodo.value=""
+
+    }
+    // console.log(inputTodo.value)
+})
+
+
+
