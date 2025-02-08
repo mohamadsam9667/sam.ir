421efb34c1eb5d9217430bf4c0957a5bff2a5ec0
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2025-02-08 21:12:40 +0330

    finish oncontextMenu

---
+++ 150-explain-of-custom-context-menu-project/source/js/script.js
@@ -0,0 +1,28 @@
+﻿console.log("sam")
+
+
+
+
+let menu=document.querySelector('.context-menu')
+
+
+console.log(menu)
+
+
+document.addEventListener('contextmenu',function(event){
+    
+    event.preventDefault()
+    menu.style.display='flex';
+
+    menu.style.left=`${event.clientX}px`;
+    menu.style.top = `${event.clientY}px`
+
+
+    console.log(event)
+})
+document.addEventListener('dblclick',function(){
+    menu.style.display='none';
+
+})
+
+
