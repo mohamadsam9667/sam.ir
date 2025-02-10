ab13d14e56339361957dff05f73def5b9d1d632e
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2025-02-10 16:08:25 +0330

    Update script.js

---
+++ 162-explain-of-maxlength-project/Source/js/script.js
@@ -0,0 +1,19 @@
+let inputText=document.querySelector('input')
+let counterIcon=document.querySelector('.counter')
+console.log(inputText,counterIcon)
+let i
+inputText.addEventListener('keyup',function(event){
+    // for (i=0,i<19,i++){
+        counterIcon.innerHTML=19-inputText.value.length
+
+    // }
+
+
+    // console.log()
+
+})
+
+
+
+
+
